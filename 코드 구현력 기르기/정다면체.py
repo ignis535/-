e91d9b7858aca508c07adb88e26e30dc9891a11e
ignis535 <ignis535@pusan.ac.kr>
2021-12-23 21:26:29 +0900

    .

---
+++ 코드 구현력 기르기/정다면체.py
@@ -0,0 +1,14 @@
+import sys
+# sys.stdin = open("input.txt", "rt")
+
+n, m = map(int, input().split())
+check = [0] * 41
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        check[i + j] += 1
+
+maximum = max(check)
+for idx, element in enumerate(check):
+    if element == maximum:
+        print(idx, end = " ")
