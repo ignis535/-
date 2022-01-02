9742dd6061cc234277512c55e846cc8cae5e24e6
ignis535 <ignis535@pusan.ac.kr>
2022-01-02 17:01:58 +0900

    뮤직비디오

---
+++ 이분 탐색(결정알고리즘) & 그리디 알고리즘/뮤직비디오.py
@@ -0,0 +1,28 @@
+import sys
+# sys.stdin = open("input.txt", "r")
+
+n, m = map(int, input().split())
+dvd = list(map(int, input().split()))
+
+def count(capacity):
+    cnt = 1
+    tot = 0
+    for x in dvd:
+        if tot + x > capacity:
+            cnt += 1
+            tot = x
+        else:
+            tot += x
+    return cnt
+
+left = 1
+right = sum(dvd)
+result = 0
+while left <= right:
+    pivot = (left + right) // 2
+    if count(pivot) <= m:
+        result = pivot
+        right = pivot - 1
+    else:
+        left = pivot + 1
+print(result)
