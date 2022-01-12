bffede72319bcc4b8ea0e6e7ec3995fb684a7a1f
ignis535 <ignis535@pusan.ac.kr>
2022-01-13 03:55:44 +0900

    수열 추측하기

---
+++ 완전탐색(백트래킹, 상태트리와 CUT EDGE), DFS 기초/수열 추측하기.py
@@ -0,0 +1,60 @@
+"""
+import sys
+# sys.stdin = open("input.txt", 'r')
+
+def dfs(level):
+    global switch
+    if switch == 1: return
+    if level == n:
+        tot = 0
+        for i in range(n):
+            tot += coef[i] * result[i]
+        if tot == f:
+            print(' '.join(map(str, result)))
+            switch = 1
+    else:
+        for i in range(1, n + 1):
+            if check[i] == 0:
+                check[i] = 1
+                result[level] = i
+                dfs(level + 1)
+                check[i] = 0
+
+if __name__ == "__main__":
+    n, f = map(int, input().split())
+    switch = 0
+    check = [0] * (n + 1)
+    result = [0] * n
+    coef = [1]
+    for r in range(n):
+        tmp = coef[-1] * (n - 1 - r) / (r + 1)
+        coef.append(tmp)
+    dfs(0)
+"""
+import sys
+# sys.stdin = open("input.txt", 'r')
+
+def dfs(level, sum):
+    global switch
+    if switch == 1: return
+    if level == n and sum == f:
+        for i in range(n):
+            print(result[i], end = ' ')
+        switch = 1
+    else:
+        for i in range(1, n + 1):
+            if check[i] == 0:
+                check[i] = 1
+                result[level] = i
+                dfs(level + 1, sum + (coef[level] * result[level]))
+                check[i] = 0
+
+if __name__ == "__main__":
+    n, f = map(int, input().split())
+    switch = 0
+    check = [0] * (n + 1)
+    result = [0] * n
+    coef = [1] * n
+    for i in range(1, n - 1):
+        coef[i] = coef[i - 1] * (n - i) / i
+    dfs(0, 0)
