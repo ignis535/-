72f3dfa7ca841b7506da2e7615894709bbc365d8
ignis535 <ignis535@pusan.ac.kr>
2022-01-13 04:53:00 +0900

    수들의 조합

---
+++ 완전탐색(백트래킹, 상태트리와 CUT EDGE), DFS 기초/수들의 조합.py
@@ -0,0 +1,46 @@
+"""
+import sys
+# sys.stdin = open("input.txt", 'r')
+
+def dfs(level, start):
+    global cnt
+    if level == k:
+        tot = 0
+        for i in range(k):
+            tot += result[i]
+        if tot % m == 0:
+            cnt += 1
+    else:
+        for i in range(start, n):
+            result[level] = arr[i]
+            dfs(level + 1, i + 1)
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+    m = int(input())
+    cnt = 0
+    result = [0] * k
+    dfs(0, 0)
+    print(cnt)
+"""
+import sys
+# sys.stdin = open("input.txt", 'r')
+
+def dfs(level, start, sum):
+    global cnt
+    if level == k:
+        if sum % m == 0:
+            cnt += 1
+    else:
+        for i in range(start, n):
+            dfs(level + 1, i + 1, sum + arr[i])
+
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+    m = int(input())
+    cnt = 0
+    dfs(0, 0, 0)
+    print(cnt)
