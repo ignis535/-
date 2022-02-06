1042e0175d8b5eacd43ee44e5bee1f0f7da37e6b
ignis535 <ignis535@pusan.ac.kr>
2022-02-06 13:48:53 +0900

    네트워크 선 자르기

--- 동적계획법/네트워크 선 자르기.py
+++ 동적계획법/네트워크 선 자르기.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         memo.append(memo[i - 2] + memo[i - 1])
 
     print(memo[n])
-"""
+
 import sys
 # sys.stdin = open("input.txt", 'r')
 
@@ -22,4 +22,40 @@ if __name__ == "__main__":
     for i in range(3, n + 1):
         memo[i] = memo[i - 1] + memo[i - 2]
 
+    print(memo[n])
+
+import sys
+# sys.stdin = open("input.txt", 'r')
+
+def dp(n):
+    if memo[n] == 0:
+        memo[n] =  dp(n - 1) + dp(n - 2)
+        return memo[n]
+    else:
+        return memo[n]
+
+if __name__ == "__main__":
+    n = int(input())
+    memo = [0] * (n + 1)
+    memo[1] = 1
+    memo[2] = 2
+    print(dp(n))
+"""
+
+import sys
+# sys.stdin = open("input.txt", 'r')
+
+def dfs(n):
+    if memo[n] != 0:
+        return memo[n]
+    if n == 1 or n == 2:
+        return n
+    else:
+        memo[n] = dfs(n - 1) + dfs(n - 2)
+        return memo[n]
+
+if __name__ == "__main__":
+    n = int(input())
+    memo = [0] * (n + 1)
+    dfs(n)
     print(memo[n])
