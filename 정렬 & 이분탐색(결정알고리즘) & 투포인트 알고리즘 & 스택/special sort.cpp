6ab17747f6cbaf4ac90ac11382f344411bcc06b5
ignis535 <ignis535@pusan.ac.kr>
2022-02-13 11:58:28 +0900

    special sort

---
+++ 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/special sort.cpp
@@ -0,0 +1,62 @@
+/*
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main(void){
+	// freopen("input.txt", "r", stdin);
+	int n, i, j, tmp;
+	scanf("%d", &n);
+	vector<int> arr(n);
+	
+	for(i = 0; i < n; i++){
+		scanf("%d", &arr[i]);
+	}
+	
+	for(i = 0; i < n - 1; i++){
+		for(j = 0; j < n - 1 - i; j++){
+			if(arr[j] * arr[j + 1] < 0 && arr[j] > arr[j + 1]){
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;	
+			}
+		}
+	}
+	
+	for(i = 0; i < n; i++){
+		printf("%d ", arr[i]);	
+	}
+	
+	return 0;
+}
+*/
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main(void){
+	// freopen("input.txt", "r", stdin);
+	int n, i, j, tmp;
+	scanf("%d", &n);
+	vector<int> arr(n);
+	
+	for(i = 0; i < n; i++){
+		scanf("%d", &arr[i]);
+	}
+	
+	for(i = 0; i < n - 1; i++){
+		for(j = 0; j < n - 1 - i; j++){
+			if(arr[j] > 0 && arr[j + 1] < 0){
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;	
+			}
+		}
+	}
+	
+	for(i = 0; i < n; i++){
+		printf("%d ", arr[i]);	
+	}
+	
+	return 0;
+}
